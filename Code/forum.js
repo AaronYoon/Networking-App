ee0b8f1b39b792006561cfdc44e8738e39b86f31
AaronYoon <55563448+AaronYoon@users.noreply.github.com>
2019-09-25 00:37:48 -0700

    Add files via upload

--- Code/forum.js
+++ Code/forum.js
@@ -1,17 +1,21 @@
-function openForm() {
+function openForum() {
   document.getElementById("myForm").style.display = "block";
 }
 function closeForum() {
   document.getElementById("myForm").style.display = "none";
 }
 
-/*
-$('#post').click(function() {
-    $('#forumbox').append($('<li class="flex-item">').text('Hello'));
-    $(this).insertAfter($('[class="flex-item"]').last());
-});
+  /*
+  $('#post').click(function() {
+      $('#forumbox').append($('<li class="flex-item">').text('Hello'));
+      $(this).insertAfter($('[class="flex-item"]').last());
+  }); 
 
-*/
+  */
+
+function openprofile() {
+  
+}
 
 function postForum(){
   $('#forumbox').append($('<li class="flex-item">').text('Hello'));
