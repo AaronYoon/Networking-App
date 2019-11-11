1482e72eececd68e9c384fe8df7a11cd48504d03
Gabriel Rivera <gabrielrivera457@gmail.com>
2019-11-10 21:42:49 -0800

    Iteration #3 files

---
+++ userProfile.js
@@ -0,0 +1,16 @@
+﻿function openForum() {
+    document.getElementById("sendBox").style.display = "block";
+}
+function closeForum() {
+    document.getElementById("sendBox").style.display = "none";
+}
+
+function sendMessage() {
+    alert("Message sent!");
+    // var text1 = document.getElementById('textarea').value;
+}
+
+function grabMessageText() {
+    var text = document.getElementById("messageArea").value;
+    return text;
+}
